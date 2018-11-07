70e832918028f778e01449844f00d9d7b48e716b
a lan <bluenowte97@gmail.com>
2018-11-07 01:57:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,23 +1,23 @@
  def my_collect(array)
-   new_collection = [] 
-  
-   i = 0 
-   while i < array.length 
+   new_collection = []
+
+   i = 0
+   while i < array.length
      new_collection << yield(array[i])
-     i += 1 
-   end 
+     i += 1
+   end
    new_collection
-end  
+end
 
 #variables for call have to be on top
 student = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 collection = ["ruby", "javascript", "python", "objective-c"]
 
 
-my_collect(student) do |name| 
-  name.split(" ").first 
-end 
+my_collect(student) do |name|
+  name.split(" ").first
+end
 
 my_collect(collection) do |lang|
-  lang.upcase 
-end 
+  lang.upcase
+end
